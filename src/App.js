aae996b1a78e7187e75b2dc9475f65d5b32e4d8b
inekeyuliam <inekemargareta20@gmail.com>
2022-11-26 17:21:09 +0700

    setup Tailwind CSS

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const npm install -D tailwindcss postcss autoprefixer = () => {
+  return (
+	<div>npm install -D tailwindcss postcss autoprefixer</div>
+  )
+}
+
+export default npm install -D tailwindcss postcss autoprefixer
--- src/index.js
+++ src/index.js
@@ -2,4 +2,5 @@ import React from 'react';
 import ReactDOM  from 'react';
 
 import App from './App';
+import '.global.css';
 ReactDOM.render(<App />, document.getElementById('root'))
